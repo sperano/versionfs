da665bb526be973d813d915d17eeefe0e734c225
Éric Spérano <eric.sperano@gmail.com>
2023-10-13 09:47:51 -0700

    path exists

--- localfs.go
+++ localfs.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/rs/zerolog/log"
 	"os"
-	"path"
+	path_ "path"
 	"sort"
 	"strings"
 	"time"
@@ -42,22 +42,22 @@ func (l *LocalFS) RegisterFileType(ftype FileType, constructor Constructor) {
 
 func (l *LocalFS) Write(file File, data []byte) (Timestamp, error) {
 	log.Info().Msgf("write file %s/%s.?.%s", file.Dir(), file.Name(), file.Ext())
-	if err := os.MkdirAll(path.Join(l.RootPath, file.Dir()), 0755); err != nil {
+	if err := os.MkdirAll(path_.Join(l.RootPath, file.Dir()), 0755); err != nil {
 		return Timestamp{}, err
 	}
 	ts := NewFromTime(time.Now())
 	filepath := Path(file, ts)
-	return ts, os.WriteFile(path.Join(l.RootPath, filepath), data, 0644)
+	return ts, os.WriteFile(path_.Join(l.RootPath, filepath), data, 0644)
 }
 
 func (l *LocalFS) Read(file File, ts Timestamp) ([]byte, error) {
 	log.Info().Msgf("read file %s/%s.%s.%s", file.Dir(), file.Name(), ts, file.Ext())
-	return os.ReadFile(path.Join(l.RootPath, Path(file, ts)))
+	return os.ReadFile(path_.Join(l.RootPath, Path(file, ts)))
 }
 
 func (l *LocalFS) Remove(file File, ts Timestamp) error {
 	log.Info().Msgf("remove file %s/%s.%s.%s", file.Dir(), file.Name(), ts, file.Ext())
-	return os.Remove(path.Join(l.RootPath, Path(file, ts)))
+	return os.Remove(path_.Join(l.RootPath, Path(file, ts)))
 }
 
 func (l *LocalFS) New(ftype FileType, args ...any) File {
@@ -69,7 +69,7 @@ func (l *LocalFS) New(ftype FileType, args ...any) File {
 }
 
 func (l *LocalFS) Versions(file File) ([]Timestamp, error) {
-	entries, err := os.ReadDir(path.Join(l.RootPath, file.Dir()))
+	entries, err := os.ReadDir(path_.Join(l.RootPath, file.Dir()))
 	if err != nil {
 		return nil, err
 	}
@@ -97,4 +97,19 @@ func (l *LocalFS) Versions(file File) ([]Timestamp, error) {
 		}
 	}
 	return versions, nil
+}
+
+func (l *LocalFS) PathExists(path string) (bool, error) {
+	_, err := os.Stat(path_.Join(l.RootPath, path))
+	if err != nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func (l *LocalFS) MkdirAll(path string, perm os.FileMode) error {
+	return os.MkdirAll(path_.Join(l.RootPath, path), perm)
 }
